a606b42030bf8f51300e46a1dceeeb7a3153d331
Przemysław Zalewski <ciunkos@gmail.com>
2017-11-23 00:58:29 +0100

    Test explicit module paths

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,7 +1,7 @@
 // @auto-index
 
-import Page from './Page'
+import Page from './Page.js'
 
-export { default as Page } from './Page'
+export { default as Page } from './Page.js'
 
 export default Page
