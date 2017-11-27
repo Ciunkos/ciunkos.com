99adde4b3f5ec646a8c261ec9ab5c42e4ddc9b56
Przemysław Zalewski <ciunkos@gmail.com>
2017-11-27 21:34:40 +0100

    Store snapshots under specific directory

---
+++ jest/moveSnapshots.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const glob = require('glob')
+
+const outputDirectory = [process.env.CIRCLE_ARTIFACTS, 'snapshots']
+  .filter(x => x)
+  .join('/')
+
+const createDir = dir => {
+  const splitPath = dir.split('/')
+  splitPath.reduce((path, subPath) => {
+    if (!fs.existsSync(path)) {
+      console.log(`Create directory at ${path}.`);
+      fs.mkdirSync(path)
+    }
+    return `${path}/${subPath}`
+  })
+}
+
+glob('src/__snapshots__/*.snap', {}, (er, files) => {
+  files.forEach(fileName => {
+    const newName = fileName
+      .replace(/__snapshots__\//g, '')
+      .replace('src/', `${outputDirectory}/`)
+
+    console.log(`Move file ${fileName} to ${newName}.`);
+    createDir(newName)
+    fs.renameSync(fileName, newName)
+  })
+
+  glob('src/__snapshots__', {}, (er, files) => {
+    files.forEach(fileName => {
+      fs.rmdirSync(fileName)
+    })
+  })
+})
