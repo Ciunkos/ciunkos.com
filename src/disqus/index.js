3a5cabab08b1fef040bb1b470c971cdede448b33
Przemysław Zalewski <ciunkos@gmail.com>
2017-12-18 22:18:01 +0100

    Apply prettier formatting

--- src/disqus/index.js
+++ src/disqus/index.js
@@ -2,9 +2,7 @@ import fetch from 'isomorphic-fetch'
 
 const forumName = 'ciunkos'
 
-const apiEndpoint = `https://disqus.com/api/3.0/forums/listThreads.json?forum=${
-  forumName
-}`
+const apiEndpoint = `https://disqus.com/api/3.0/forums/listThreads.json?forum=${forumName}`
 const key = 'nJdchLe13iM0uRIx0oRmxVqZpoCW6oEU4Ak3yfhi76LmJEb0nrbbZ4GMe2XJsUyQ'
 
 const enpoint = `${apiEndpoint}&api_key=${key}`
