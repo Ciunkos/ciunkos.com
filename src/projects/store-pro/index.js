0e2fb645b5c5792ed34d8bb3dc9a905fdeb429ab
Przemysław Zalewski <ciunkos@gmail.com>
2021-03-01 22:04:54 +0100

    Remove legacy app links and content

--- src/projects/store-pro/index.js
+++ src/projects/store-pro/index.js
@@ -1,24 +1,21 @@
 // @auto-index
 
 import cover from './cover.jpg'
-import Description from './Description.txt'
+import description from './description.txt'
 import features from './features.json'
 import icon from './icon.jpg'
-import link from './link.txt'
 import screenshots from './screenshots'
 
 export { default as cover } from './cover.jpg'
-export { default as Description } from './Description.txt'
+export { default as description } from './description.txt'
 export { default as features } from './features.json'
 export { default as icon } from './icon.jpg'
-export { default as link } from './link.txt'
 export { default as screenshots } from './screenshots'
 
 export default {
   cover,
-  Description,
+  description,
   features,
   icon,
-  link,
   screenshots
 }
--- src/projects/testy-prawo-jazdy/index.js
+++ src/projects/testy-prawo-jazdy/index.js
@@ -1,15 +1,12 @@
 // @auto-index
 
 import icon from './icon.jpg'
-import link from './link.txt'
 import screenshots from './screenshots'
 
 export { default as icon } from './icon.jpg'
-export { default as link } from './link.txt'
 export { default as screenshots } from './screenshots'
 
 export default {
   icon,
-  link,
   screenshots
 }
