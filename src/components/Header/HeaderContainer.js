1ca6dfc2798df552c4a909755da8477eb5f56010
Przemysław Zalewski <ciunkos@gmail.com>
2022-06-03 10:23:31 +0200

    Bring back smooth header fade out

--- src/components/Header/HeaderContainer.js
+++ src/components/Header/HeaderContainer.js
@@ -106,7 +106,7 @@ export default class HeaderContainer extends React.PureComponent {
         prevPosition = position
       }
       if (prevTop !== top) {
-        headerElement.style.top = `${Math.max(0, top)}px`
+        headerElement.style.top = `${top}px`
         prevTop = top
       }
       // end of manual DOM manipulation
