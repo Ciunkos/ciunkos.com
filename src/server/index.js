95daeaafe4d0ae02f98913ab6e216e85bacf0acd
Przemysław Zalewski <ciunkos@gmail.com>
2026-09-02 23:13:06 +0200

    Add sitemaps

--- src/server/index.js
+++ src/server/index.js
@@ -12,20 +12,26 @@ import { match } from '../routes'
 import template from './template'
 
 const routes = [
-  ['/', 'index.html'],
-  ['/about'],
-  ['/apps'],
-  ['/creating-contact-forms-with-nodemailer-and-react'],
-  ['/data-as-code-modules'],
-  ['/debugging-javascript-the-basics'],
-  ['/resume'],
-  ['/traversable-objects-in-java-script'],
-  ['/not-found', '404.html']
+  { pathname: '/', alias: 'index.html', includeInSitemap: true },
+  { pathname: '/about', includeInSitemap: true },
+  { pathname: '/apps', includeInSitemap: true },
+  {
+    pathname: '/creating-contact-forms-with-nodemailer-and-react',
+    includeInSitemap: true
+  },
+  { pathname: '/data-as-code-modules', includeInSitemap: true },
+  { pathname: '/debugging-javascript-the-basics', includeInSitemap: true },
+  { pathname: '/resume', includeInSitemap: true },
+  { pathname: '/traversable-objects-in-java-script', includeInSitemap: true },
+  { pathname: '/not-found', alias: '404.html', includeInSitemap: true }
 ]
 
 const main = async () => {
   try {
-    for (const [pathname, alias = `${pathname.substring(1)}.html`] of routes) {
+    for (const {
+      pathname,
+      alias = `${pathname.substring(1)}.html`
+    } of routes) {
       const location = { pathname }
 
       const route = match(pathname)
@@ -36,8 +42,18 @@ const main = async () => {
       const helmet = Helmet.renderStatic()
       const html = template(renderedComponent, { ...helmet, pathname })
 
-      await writeFile(`./dist/${alias}`, html)
+      await writeFile(`./dist/${alias}`, html, 'utf-8')
     }
+
+    const sitemapUrls = routes
+      .filter(route => route.includeInSitemap)
+      .map(
+        route => `<url><loc>https://ciunkos.com${route.pathname}</loc></url>`
+      )
+
+    const sitemapXml = `<?xml version="1.0" encoding="utf-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">${sitemapUrls.join('')}</urlset>`
+
+    await writeFile('./dist/sitemap.xml', sitemapXml, 'utf-8')
 
     process.exit(0)
   } catch (error) {
